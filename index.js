d4f0955e50d06a33e68e1930ead9836173db5324
VasiliyIsaichkin <vasiliy@isaichkin.ru>
2019-08-22 15:11:29 +0300

    Megaupdate / break-changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {WebApiServer, WebApiClient} = require('osmium-webapi');
-const {Crypt, tools} = require('osmium-crypt');
+const {Crypt, oTools} = require('osmium-crypt');
 
 class IApiServer extends WebApiServer {
 	constructor(io, options, name, authProvider) {
@@ -18,7 +18,7 @@ class IApiClient extends WebApiClient {
 			nameReqCmdRet: 'r'
 		});
 		super(socket, options);
-		this.options.iApiVersion = 5;
+		this.options.iApiVersion = 8;
 
 		this.cryptor = new Crypt(this.options);
 		this.remoteName = false;
@@ -29,39 +29,37 @@ class IApiClient extends WebApiClient {
 			socket.emit(`${this.options.prefix}${this.options.nameReqCmdRet}`, {name, authMeta});
 		});
 
-		this.registerMiddlewareOut(async (packet, socket, _, local) => {
-			if (local) return packet;
-			await this.requestRemoteName(socket);
+		const encrypt = async ($socket, $packet, $id) => {
+			if (this.isLocal) return;
+			await this.requestRemoteName($socket);
 
-			const id = packet.id;
-			delete packet.id;
 			const sharedKey = authProvider.get(this.options.isServer, this.remoteName, name, this.authMeta, this.options.keySalt);
-			const payload = await this.cryptor.encrypt(sharedKey, packet, id + '|' + socket.id,
+			const payload = await this.cryptor.encrypt(sharedKey, this.filterPacket($packet), $id + '|' + $socket.id,
 				{v: this.options.iApiVersion, i: name});
 
-			return {
-				version : packet.version,
-				id,
-				name,
-				args    : payload,
-				metadata: packet.metadata
-			};
-		});
+			$packet.args = [payload];
+		};
 
-		this.registerMiddlewareInc(async (packet, socket, _, local) => {
-			if (local) return packet;
-			await this.requestRemoteName(socket);
+		const decrypt = async ($packet, $socket, $args) => {
+			if (this.isLocal) return;
+			await this.requestRemoteName($socket);
 
 			const sharedKey = authProvider.get(this.options.isServer, this.remoteName, name, this.authMeta, this.options.keySalt,
 				(userData) => userData.v === this.options.iApiVersion && userData.i === this.remoteName);
-			const data = await this.cryptor.decrypt(sharedKey, packet.args, true);
+			const data = await this.cryptor.decrypt(sharedKey, $args[0], true);
+
 			if (!data || !data.id) return null;
-			packet = data.payload;
+			Object.assign($packet, data.payload);
+
 			const idArr = data.id.split('|');
 			if (idArr[1] !== socket.id) return null;
-			packet.id = idArr[0];
-			return packet;
-		});
+		};
+
+
+		this.middlewareIncBefore(this.PRIORITY.FIRST - 1, decrypt);
+		this.middlewareIncAfter(this.PRIORITY.LAST + 1, encrypt);
+		this.middlewareOutBefore(this.PRIORITY.LAST + 1, encrypt);
+		this.middlewareOutAfter(this.PRIORITY.FIRST - 1, decrypt);
 	}
 
 	async requestRemoteName(socket) {
